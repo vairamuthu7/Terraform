7c5f5e937dc6bf489cfeb2c22a4712c6d4cce08d
vairamuthu7 <87692876+vairamuthu7@users.noreply.github.com>
2025-03-19 12:46:57 +0530

    Update EC2-Simple.tf

--- EC2-Simple.tf
+++ EC2-Simple.tf
@@ -1,9 +1,9 @@
 provider "aws" {
-  region = "region"
+  region = "ap-south-1"
 }
 resource "aws_instance" "Demo" {
   ami           = "ami-09773b29dffbef1f2"
-  instance_type = "t2.micro"
+  instance_type = "t4g.micro"
   key_name = "win-mar-key"
   tags = {
     Name = "dummy"
